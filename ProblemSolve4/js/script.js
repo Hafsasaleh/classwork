0bc13828432c1a017c6b2d47e08257ee3c38f047
Hafsasaleh <141514124+Hafsasaleh@users.noreply.github.com>
2023-08-18 02:27:22 +0400

    Add files via upload

---
+++ ProblemSolve4/js/script.js
@@ -0,0 +1,12 @@
+function check(user){
+    if(user.length %2===0){
+        return"girl";
+    }
+    else{
+        return "boy";
+
+    }
+}
+
+console.log(check("Ahmed"))
+console.log(check("Sara"))
